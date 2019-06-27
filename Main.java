2716fad86671379c2ea5e558d7eafc77d3e4f190
jasmine DE-AGRELA <jde-agr@student.wethinkcode.co.za>
2019-06-27 10:14:39 +0200

    Basic file reader implementation

---
+++ Main.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        if (args.length == 1) {
+            try {
+                BufferedReader br = new BufferedReader(new FileReader(args[0]));
+                try {
+                    for (String line; (line = br.readLine()) != null;) {
+                        System.out.println(line);
+                    }
+                }
+                catch (IOException e) {
+                    System.out.println(e);
+                }
+            }
+            catch (FileNotFoundException e) {
+                 System.out.println(e);
+            }
+        } else {
+            System.out.println("Must have one argument");
+        }
+    }
+}
